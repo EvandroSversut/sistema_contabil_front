a99a1ab32078a582b589af47c344425ab27f544f
Evandro <svecontabilidade@gmail.com>
2025-05-11 09:50:10 -0300

    Criado a tela de login e cadastro

---
+++ src/app/pages/cadastro/cadastro.component.ts
@@ -0,0 +1,36 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { RouterModule, Router } from '@angular/router';
+import { HttpClient, HttpClientModule } from '@angular/common/http';
+
+@Component({
+  selector: 'app-cadastro',
+  standalone: true,
+  imports: [CommonModule, FormsModule, RouterModule, HttpClientModule],
+  templateUrl: './cadastro.component.html',
+  styleUrls: ['./cadastro.component.css']
+})
+export class CadastroComponent {
+  usuario = {
+    login: '',
+    senha: ''
+  };
+
+  mensagem = '';
+
+  constructor(private http: HttpClient, private router: Router) {}
+
+  cadastrar() {
+    this.http.post('http://localhost:8080/api/login/criar-usuario', this.usuario, { responseType: 'text' })
+      .subscribe({
+        next: (res) => {
+          this.mensagem = res;
+          if (res === 'Usuário cadastrado com sucesso!') {
+            setTimeout(() => this.router.navigate(['/']), 1500);
+          }
+        },
+        error: () => this.mensagem = 'Erro ao cadastrar'
+      });
+  }
+}
--- src/main.ts
+++ src/main.ts
@@ -1,14 +1,16 @@
 import { bootstrapApplication } from '@angular/platform-browser';
-import { appConfig } from './app/app.config';
 import { AppComponent } from './app/app.component';
-import { routes } from './app/app.routes';
 import { provideHttpClient } from '@angular/common/http';
 import { provideRouter } from '@angular/router';
-
+import { CadastroComponent } from './app/pages/cadastro/cadastro.component';
+import { LoginComponent } from './app/pages/login/login.component';
 
 bootstrapApplication(AppComponent, {
   providers: [
-    provideRouter(routes),
-    provideHttpClient()
+    provideHttpClient(),
+    provideRouter([
+      { path: '', component: LoginComponent },
+      { path: 'cadastro', component: CadastroComponent }
+    ])
   ]
 });
