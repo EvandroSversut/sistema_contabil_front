c265c37afe0a734b9cf2b333f5e1498c8127cce6
Evandro <svecontabilidade@gmail.com>
2025-05-29 07:16:51 -0300

    diversos - 29052025

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { MenuComponent } from './menu/menu.component';
 import { AuthService } from './services/auth.service';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, MenuComponent],
+  imports: [RouterOutlet, MenuComponent, CommonModule],
   templateUrl: './app.component.html', // ✅ Usa o HTML externo
   styleUrls: ['./app.component.css']    // (opcional se tiver CSS)
 })
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,10 +3,16 @@ import { LoginComponent } from './login/login.component';
 import { HomeComponent } from './home/home.component';
 import { PessoaFisicaComponent } from './components/pessoa-fisica/pessoa-fisica.component';
 import { PessoajuridicaComponent } from './components/pessoa-juridica/pessoa-juridica.component';
+import { authGuard } from './guard/auth.guard';
+import { UsuarioComponent } from './components/usuario/usuario.component';
 
 export const routes: Routes = [
+
+  //  Rotas liberadas:
   { path: '', component: LoginComponent },
-  { path: 'home', component: HomeComponent },
-  { path: 'pessoa-fisica', component: PessoaFisicaComponent },
-  { path: 'pessoa-juridica', component: PessoajuridicaComponent },
+   { path: 'usuario', component: UsuarioComponent },
+  // 🔐 Rotas protegidas:
+  { path: 'home', component: HomeComponent, canActivate: [authGuard] },
+ { path: 'pessoa-fisica', component: PessoaFisicaComponent, canActivate: [authGuard] },
+  { path: 'pessoa-juridica', component: PessoajuridicaComponent, canActivate: [authGuard] },
 ];
